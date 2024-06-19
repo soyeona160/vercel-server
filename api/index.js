8b9e113f9a42d076c7fe9f2d1f8a743a244d34f9
Yeona <soyeona160@gmail.com>
2024-06-19 12:11:13 +0900

    '회원가입 로직 변경, cors'

--- api/index.js
+++ api/index.js
@@ -3,21 +3,33 @@ const mongoose = require("mongoose")
 const app = express();
 const User = require('./User')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 dotenv.config({ path: '../.env' })
+
+const corsOptions = {
+    origin: '*', // 해당 URL 주소만 요청을 허락함 
+    credentials: true // 사용자 인증이 필요한 리소스를 요청할 수 있도록 허용함
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
 
 console.log("몽고아틀라스 주소: ", process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("데이터베이스 연결 성공 !!"))
 .catch(e => console.log(`데이터베이스 연결 실패 ${e}`))
 
+
+
 app.post("/user", (req, res) => {
     // user 데이터 생성 테스트
+    console.log(req.body)
     const user = new User({
-        name: '사용자',
-        email: 'user123@gmail.com',
-        userId: 'user',
-        password: 'abcde123@' 
+        name: req.body.name,
+        email: req.body.email,
+        userId: req.body.userId,
+        password: req.body.password  
     })
     user.save()
     .then(() => {
@@ -25,7 +37,7 @@ app.post("/user", (req, res) => {
         res.json({ newUser: user })
     })
     .catch(e => {
-        // console.log(e)
+        console.log(e)
         res.status(400).json({ code: 400, message: 'Invalid User Data'})
     })
 })
